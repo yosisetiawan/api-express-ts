f9f17093f73f7c6dfb82b7447f2d4a121f489f83
Yosi Setiawan <antonioyosie@gmail.com>
2020-03-10 22:21:04 +0700

    fixing bug in middleware

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -1,29 +1,26 @@
 import { Request, Response, NextFunction} from 'express'
 import jwt from 'jsonwebtoken'
 
-export const auth = ( res:Response, req:Request, next: NextFunction) : any => {
+export const auth = (req: Request, res:Response, next:NextFunction): any => {
     if(!req.headers.authorization){
-        return res.status(401).send({
-            status: res.statusCode,
-            messages: 'You not authorization'
-        })
+        return res.status(401).send("Tidak Authenfikasi")
     }
 
-    let key = process.env.JWT_KEY ||'secret'
-    const token:string = req.headers.authorization.split(" ")[1]
+    let secretKey = process.env.JWT_KEY || 'secret';
+    const token: string = req.headers.authorization.split(" ")[1];
 
     try{
-        const credential: string | object = jwt.verify(token, key)
+        const credential: string | object = jwt.verify(token, secretKey)
 
         if(credential){
             next();
         }
 
-        return res.status(401).send({
-            status: res.statusCode,
-            messages: 'Token Invalid'
+        return res.status(401).send('Token Invalid')
+    }catch(err){
+        return res.send({
+            messages:'Terjadi Error',
+            hint: err
         })
-    }catch (err){
-        return res.send(err)
     }
 }
